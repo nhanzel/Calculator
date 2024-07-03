e89f53c8c41c5b7175d454fb2711131b970472f0
Nathan Hanzel <nathan.hanzel@gmail.com>
2024-07-02 18:53:58 -0600

    Added work energy theorem

--- equations.py
+++ equations.py
@@ -1,6 +1,5 @@
 import numpy as np
 import sympy as sp
-import inspect
 
 def impulse_momentum(m, v1, v2, t, fnet):
     if m == "?":
@@ -15,6 +14,26 @@ def impulse_momentum(m, v1, v2, t, fnet):
         fnet = sp.Symbol("fnet")
     eq = -(fnet * t) + (m * (v2 - v1))
     return sp.solve(eq)
+
+def work_energy(m, vi, vf, x, fnet):
+    sign_check = False # required since we are dealing with square roots and sympy wants to use the negative
+    if m == "?":
+        m = sp.Symbol("m")
+    elif vi == "?":
+        vi = sp.Symbol("vi")
+        sign_check = True
+    elif vf == "?":
+        vf = sp.Symbol("vf")
+        sign_check = True
+    elif x == "?":
+        x = sp.Symbol("x")
+    elif fnet == "?":
+        fnet = sp.Symbol("fnet")
+    eq = -(fnet * x) + (0.5 * m * (vf**2 - vi**2))
+    result = sp.solve(eq)
+    if sign_check:
+        result[0] = abs(result[0])
+    return result
 
 # HELPER FUNCTIONS
 
--- main.py
+++ main.py
@@ -3,26 +3,45 @@ from equations import *
 running = True
 
 def operations():
-    return "1. Impulse Momentum Equation (working equation)"
+    return """1. Impulse Momentum Theorem (working equation)\n
+2. Work Energy Theorem (working equation)\n"""
 
 while running:
     print(operations())
     operation = input("Enter operation: ")
+    # Impulse Momentum Theorem
     if operation == "1":
         params = {
             "m": 0.0,
-            "v1": 0.0,
-            "v2": 0.0,
+            "vi": 0.0,
+            "vf": 0.0,
             "t": 0.0,
             "fnet": 0.0
         }
         params['m'] = get_input_param("Enter mass (m): ")
-        params['v1'] = get_input_param("Enter initial velocity (vi): ")
-        params['v2'] = get_input_param("Enter final velocity (vf): ")
+        params['vi'] = get_input_param("Enter initial velocity (vi): ")
+        params['vf'] = get_input_param("Enter final velocity (vf): ")
         params['t'] = get_input_param("Enter time (t): ")
         params['fnet'] = get_input_param("Enter net force (fnet): ")
         
-        answer = str(impulse_momentum(params['m'], params['v1'], params['v2'], params['t'], params['fnet'])[0])
+        answer = str(impulse_momentum(params['m'], params['vi'], params['vf'], params['t'], params['fnet'])[0])
+        output_answer(answer, params)
+    # Work Energy Theorem
+    if operation == "2":
+        params = {
+            "m": 0.0,
+            "vi": 0.0,
+            "vf": 0.0,
+            "x": 0.0,
+            "fnet": 0.0
+        }
+        params['m'] = get_input_param("Enter mass (m): ")
+        params['vi'] = get_input_param("Enter initial velocity (vi): ")
+        params['vf'] = get_input_param("Enter final velocity (vf): ")
+        params['x'] = get_input_param("Enter distance (x): ")
+        params['fnet'] = get_input_param("Enter net force (fnet): ")
+
+        answer = str(work_energy(params['m'], params['vi'], params['vf'], params['x'], params['fnet'])[0])
         output_answer(answer, params)
     else:
         print("Invalid operation")
